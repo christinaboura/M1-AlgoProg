31d4e915d32fea7a7c0a1f8e38c08cfb4e59beee
AlexGelin <32389227+AlexGelin@users.noreply.github.com>
2018-02-06 16:59:23 +0100

    Update TP3Exo4-1_fibonacci.py

--- solutions/TP3Exo4-1_fibonacci.py
+++ solutions/TP3Exo4-1_fibonacci.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def fibonacci(n) :
-    
     L = [0, 1]
-    
     for i in range (2, n) :
         L.append(L[i-1] + L[i-2])
     dico = {}
@@ -11,12 +9,7 @@ def fibonacci(n) :
         dico[L[i]] = i
     return dico
 
-
 dico = fibonacci(10)
 for k,v in dico.items():
     print (k,v)
 print(dico)
-
-
-
-
