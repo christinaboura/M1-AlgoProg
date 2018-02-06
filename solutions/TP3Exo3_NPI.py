37d86a6a493d99e4c5488d1f97f5009980bb1259
AlexGelin <32389227+AlexGelin@users.noreply.github.com>
2018-02-06 16:49:40 +0100

    Update TP3Exo3_NPI.py

--- solutions/TP3Exo3_NPI.py
+++ solutions/TP3Exo3_NPI.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
 def estOperateur(c) :
-    ''' Fonction qui renvoie True si c égal un des quatre opérateurs et False sinon '''
+    '''fonction qui renvoie True si c est égal à un des quatre opérateurs et False sinon'''
     if c == '+' or c == '-' or c == '*' or c == '/' :
         return True
     else :
         return False
 
-
-
 def calcul(op, n, m) :
-    ''' Fonction renvoyant le resultat n op m '''
+    '''fonction renvoyant le resultat n op m'''
     if op == '+' :
         return n + m
     elif op == '-' :
@@ -20,23 +18,20 @@ def calcul(op, n, m) :
     elif op == '/' :
         return n / m
 
-
 def evaluation(s) :
-    ''' Evaluation de l'expession à l'aide d'une pile '''
+    '''évaluation de l'expession à l'aide d'une pile'''
     P = []
     for i in range(0, len(s)) : # parcours de tous les caractères de la chaîne
-        if estOperateur(s[i]) == False : # s'il ne s'agit pas d'un opérateurs
+        if estOperateur(s[i]) == False : # s'il ne s'agit pas d'un opérateur
             P.append(float(s[i])) # alors on empile
         else : #si c'est un opérateur
-            n = P.pop() # on dépile les deux derniers caracteres
+            n = P.pop() # on dépile les deux derniers caractères
             m = P.pop()
             P.append(calcul(s[i], float(m), float(n))) # et on empile le résultat de l'opération
     res = P.pop() # le résultat final se trouve au sommet de la pile
     return res
 
-
 #s = "12+4*3+"
 s = "583-*3*31-2*3/+"
 res = evaluation(s)
-print("Le resultat est ", res)
-
+print "Le resultat est", res
