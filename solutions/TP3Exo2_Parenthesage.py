0c63d3c3b340ee190ba41bb683c092bc3f5f6d34
AlexGelin <32389227+AlexGelin@users.noreply.github.com>
2018-02-06 16:30:50 +0100

    Update TP3Exo2_Parenthesage.py

--- solutions/TP3Exo2_Parenthesage.py
+++ solutions/TP3Exo2_Parenthesage.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def estOuvrante(c) :
-    ''' fonction qui renvoie True si le caractère passé en paramètre est un symbole ouvrant, et renvoie False sinon '''
+    ''' fonction qui renvoie True si le caractère passé en paramètre est un symbole ouvrant et renvoie False sinon '''
     if c == '(' or c == '[' or c == '{' :
         return True
     else :
         return False
-
-
 
 def fermante(c) :
     ''' Fonction qui renvoie le symbole fermant correspondant '''
@@ -20,7 +18,6 @@ def fermante(c) :
     else :
         return None
 
-
 def verification(s) :
     ''' Fonction qui vérifie à l'aide d'une pile P si le parenthésage est correct '''
     P = []
@@ -28,17 +25,17 @@ def verification(s) :
         if(estOuvrante(s[i])) :
             P.append(fermante(s[i])) # si s[i] est un symbole ouvrant on empile le symbole fermant correspondant
         else :
-            if s[i] == P[len(P) - 1] : # si s[i] égal le caractere se trouvant au sommet de la pile
+            if s[i] == P[len(P) - 1] : # si s[i] est égal au caractere se trouvant au sommet de la pile
                 P.pop() # on dépile le caractere
-    if not P : # si la pile à la fin est vide alors le parenthesage est correct
+    if not P : # si la pile est vide à la fin alors le parenthesage est correct
         return True
     else :
         return False
 
 
-s = "(Université de Versailles (UVSQ))"
+s = "[La (petite) maison [celle à côté du (grand) sapin (très vert {et moche})]
+ est en feu.]"
 if(verification(s)) :
     print("Parenthesage correct")
 else :
-    print("Parenthesage pas correct")
-
+    print("Parenthesage incorrect")
