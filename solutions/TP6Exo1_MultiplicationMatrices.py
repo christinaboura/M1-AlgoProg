34ac2567d72d2fea53912bb238b9de11670f9480
AlexGelin <32389227+AlexGelin@users.noreply.github.com>
2018-03-07 17:19:20 +0100

    Update TP6Exo1_MultiplicationMatrices.py

--- solutions/TP6Exo1_MultiplicationMatrices.py
+++ solutions/TP6Exo1_MultiplicationMatrices.py
@@ -1,43 +1,43 @@
-def printPyramide(M, n) :
-    i = 1
-    for j in range(n, 1, -1) :
-        l = j
-        print(" "*(2*j), end = " ")
-        for k in range(1, i+1) :
-            print(M[k][l], end = " ")
-            l = l+ 1
-        print()
-        i = i + 1
+#-*- coding: utf-8 -*-
 
-
-def printMatrix(M, n) :
-    for i in range(0, n) :
-        for j in range(i, n) :
-            print("i = ", i,  "j = ", j, ": m = ", M[i][j])
-        print(" ")
-
-def ordreMatrices(P,l) :
-    M = [[0 for i in range(l+1)] for i in range(l+1)]
-    S = [[0 for i in range(l+1)] for i in range(l+1)]
-    n = len(P) - 1
+def ordreMatrices(P):
+    n = len(P)-1
+    M = [[0 for i in range(n+1)] for i in range(n+1)]
+    S = [[0 for i in range(n+1)] for i in range(n+1)]
     
-    for i in range(1, n+1) :
+    for i in range(n):
         M[i][i] = 0
-    for l in range(2, n+1) :
-        for i in range(1, n - l + 2) :
-            j = i + l - 1
-            M[i][j] = 1591510100 # nombre aléatoire très grand
-            for k in range(i,j) :
+    for l in range(2, n+1):
+        for i in range(1, n-l+2):
+            j = i+l-1
+            M[i][j] = 4294967296 #2^32-1 considéré comme très grand
+            for k in range(i,j):
                 q = M[i][k] + M[k+1][j] + P[i-1]*P[k]*P[j]
-                if q < M[i][j] :
+                if q < M[i][j]:
                     M[i][j] = q
                     S[i][j] = k
-    L = [M,S]
-    return L
+    return [M,S]
+
+def printMatrix(M, n):
+    for i in range(1,n):
+        for j in range(i+1, n):
+            print("i = ", i,  "j = ", j, ": m = ", M[i][j])
+        print()
 
 P = [5, 10, 3, 12, 5, 50, 6]
 
-L = ordreMatrices(P, len(P)+1)
+L = ordreMatrices(P)
+#printMatrix(L[0], len(P))
+print(L[0][1][len(P)-1])
 
-printPyramide(L[0], len(P)-1)
-printMatrix(L[1], len(P))
+def printParentheses(S,i,j):
+    if i==j:
+        print("A{0}".format(i), end='')
+    else:
+        print("(", end='')
+        printParentheses(S,i,S[i][j])
+        printParentheses(S,S[i][j]+1,j)
+        print(")", end='')
+
+printParentheses(L[1],1,len(P)-1)
+print()
