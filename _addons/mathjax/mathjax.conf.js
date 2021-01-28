63033a4b4b5612f784f9fc381739b33f1638df4b
AliceEtBob <christina.boura@uvsq.fr>
2021-01-28 14:40:40 +0100

    modifying mathjax configuration

--- _addons/mathjax/mathjax.conf.js
+++ _addons/mathjax/mathjax.conf.js
@@ -1,24 +1,8 @@
-(function() {
-    config = {
-	config: ["MMLorHTML.js"],
-	jax: ["input/TeX","input/AsciiMath","output/HTML-CSS","output/NativeMML"],
-	extensions: ["MathMenu.js","MathZoom.js"],
-	TeX: {
-            extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"],
-	}
-    };
-    
-    var opt = eLeMentS.page.addons.mathjax.options;
-    if (opt) {
-	for (var p in opt) {
-	    if (p == 'TeX') {
-		for (var q in opt.TeX)
-		    config.TeX[q] = opt.TeX[q];
-	    } else {
-		config[p] = opt[p];
-	    }
-	}
+window.MathJax = {
+    config: ["MMLorHTML.js"],
+    jax: ["input/TeX","input/AsciiMath","output/HTML-CSS","output/NativeMML"],
+    extensions: ["tex2jax.js","MathMenu.js","MathZoom.js"],
+    TeX: {
+        extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"],
     }
-
-    window.MathJax = config;
-})();
+};
